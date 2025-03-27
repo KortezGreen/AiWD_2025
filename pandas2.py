f8808478bec17777822a0968831b04a8c7c2901b
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2025-03-27 12:15:05 +0100

    Update pandas2.py

--- pandas2.py
+++ pandas2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # data = pd.read_excel("./datasets/imiona.xlsx")
 # wynik = data.groupby("Rok")["Liczba"].sum() / 1000
 # print(wynik)
-# wynik.plot(figsize=(6, 6), ylabel="liczba narodzin [tys.]", xticks=wynik.index, rot=45, grid=True, title="Tytul")
+# wynik.plot(figsize=(6, 6), ylabel="liczba narodzin [tys.]", xticks=wynik.index, rot=45, grid=True, title="Tytuł")
 # plt.show()
 
 # zad 2
@@ -23,13 +23,13 @@ import matplotlib.pyplot as plt
 # wynik = data.groupby(["Rok","Plec"])["Liczba"].sum().sort_index(ascending=False).head(10).groupby("Plec").sum()
 # wynik2= data[data["Rok"]>2012].groupby("Plec")["Liczba"]
 # print(wynik2)
-# wynik.plot(kind="pie",autopct="%.2f %%", ylabel="liczba urodzeń",title="całkowita liczb urodzen w latach 2013-2017")
+# wynik.plot(kind="pie",autopct="%.2f %%", ylabel="liczba urodzeń",title="całkowita liczb urodzeń w latach 2013-2017")
 # plt.show()
 
 # zad 4
 
 # data=pd.read_csv("./datasets/zamowienia.csv", sep=";")
 # wynik = data.groupby("Sprzedawca")["idZamowienia"].count()
-# wynik.plot.barh(title="Liczba zamowien dla poszczegolnych sprzedawców")
+# wynik.plot.barh(title="Liczba zamowień dla poszczególnych sprzedawców")
 # print(wynik)
 # plt.show()
