f13a0c69cffc30a9d14e8e0cbc5fd4eef3229847
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2025-03-27 12:51:33 +0100

    Update pandas2.py

--- pandas2.py
+++ pandas2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # data = pd.read_excel("./datasets/imiona.xlsx")
 # wynik = data.groupby("Rok")["Liczba"].sum() / 1000
 # print(wynik)
-# wynik.plot(figsize=(6, 6), ylabel="liczba narodzin [tys.]", xticks=wynik.index, rot=45, grid=True, title="Tytuł")
+# wynik.plot(figsize=(8, 6), ylabel="liczba narodzin [tys.]", xticks=wynik.index, rot=45, grid=True, title="Liczba narodzin w każdym roku")
 # plt.show()
 
 # zad 2
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 # data = pd.read_excel("./datasets/imiona.xlsx")
 # wynik = data.groupby("Plec")["Liczba"].sum()
 # print(wynik)
-# wynik.plot(kind="bar", rot=0,title="całkowita liczba urodzin", ylabel="liczba urodzin")
+# wynik.plot(kind="bar", rot=0,title="Całkowita liczba urodzonych chłopców i dziewczynek", ylabel="liczba narodzin")
 # plt.show()
 
 # zad 3
@@ -23,13 +23,14 @@ import matplotlib.pyplot as plt
 # wynik = data.groupby(["Rok","Plec"])["Liczba"].sum().sort_index(ascending=False).head(10).groupby("Plec").sum()
 # wynik2= data[data["Rok"]>2012].groupby("Plec")["Liczba"]
 # print(wynik2)
-# wynik.plot(kind="pie",autopct="%.2f %%", ylabel="liczba urodzeń",title="całkowita liczb urodzeń w latach 2013-2017")
+# wynik.plot(kind="pie",autopct="%.2f %%", ylabel="liczba urodzeń",title="Całkowita liczb urodzonych chłopców i dziewczynek w latach 2013-2017")
 # plt.show()
 
 # zad 4
 
 # data=pd.read_csv("./datasets/zamowienia.csv", sep=";")
 # wynik = data.groupby("Sprzedawca")["idZamowienia"].count()
-# wynik.plot.barh(title="Liczba zamowień dla poszczególnych sprzedawców")
+# kolory = list(['red', 'blue', 'green', 'yellow', 'pink', 'orange', 'brown', 'purple', 'gray'])
+# wynik.plot.barh(color=kolory,figsize=(8, 6),rot=45,title="Liczba zamówień dla poszczególnych sprzedawców",ylabel='Sprzedawca',xlabel='Liczba zamówień')
 # print(wynik)
 # plt.show()
