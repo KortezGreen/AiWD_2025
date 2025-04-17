ab13be4850d4a2a432b7f64257d87ab577e6f291
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2025-04-17 12:46:15 +0200

    Update plt1_2.py

--- plt1_2.py
+++ plt1_2.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import pandas as pd
+
+
+#zad 6
+
+data = pd.read_excel("imiona.xlsx")
+# fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+
+x1 = data['Plec']
+y1 = data['Liczba']
+c = ['pink', 'blue']
+
+plt.bar(x1, y1, color=c)
+plt.ylabel('Ilość narodzin')
+plt.xlabel('Płeć')
+plt.show()
+
 
